e11bf0c4d57e7c47540ff3d08eebb4db56f1f575
pgao4 <pgao4@ualberta.ca>
2022-08-14 21:28:27 -0600

    add validators

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,16 @@
+const AWS = require('aws-sdk')
+
+AWS.config.update({
+   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+   secresAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+   region: process.env.AWS_REGION
+})
+
+const ses = new AWS.SES({
+   apiVersion: '2010-12-01'
+})
+
 exports.register = (req, res)=> {
-    res.json({
-        data: 'you hit register endpoint middleware... using controllers'
-    })
+   //console.log('REGISTER CONTROLLER', req.body)
+   
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-//import from controllers
-const {register} = require('../controllers/auth')
+// import validators
+const { userRegisterValidator } = require("../validators/auth");
+const { runValidation } = require("../validators");
 
-router.get('/register', register)
+// import from controllers
+const { register } = require("../controllers/auth");
+
+router.post("/register", userRegisterValidator, runValidation, register);
 
 module.exports = router;
