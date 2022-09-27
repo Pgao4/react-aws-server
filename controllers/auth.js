5c8f69cdc4f27c92d36729bb39d249c1dc85ec74
pgao4 <pgao4@ualberta.ca>
2022-09-26 21:46:34 -0600

    update email.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,7 @@
 const AWS = require('aws-sdk')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const { registerEmailParams} = require('../helpers/email')
 
 AWS.config.update({
    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -12,5 +15,38 @@ const ses = new AWS.SES({
 
 exports.register = (req, res)=> {
    //console.log('REGISTER CONTROLLER', req.body)
-   
+   const {name, email, password} = req.body
+   //check if user exists in our db
+   User.findOne({ email }).exec((err, user) => {
+         if(err) {
+            return res.status(400).json({
+               error: 'Email is taken'
+            })
+         }
+         // generate taken with user name email and password
+         const token = jwt.sign({name, email, password}, process.env.JWT_ACCOUNT_ACTIVATION, {
+            expiresIn: '10m'
+         })
+
+         // send email
+        const params = registerEmailParams(email, token)
+
+         const sendEmailOnRegister = ses.sendEmail(params).promise()
+
+         sendEmailOnRegister
+            .then(data => {
+               console.log('email submitted to SES', data)
+               res.json({
+                  message: `Email has been sent to ${email}, Follow the instructions to complete your registration`
+               })
+            })
+            .catch( error => {
+               console.log('ses email on register', error);
+               res.json({
+                  message: 'We could not verify your email, Please try again'
+               })
+            })
+         })
+
 }
+
