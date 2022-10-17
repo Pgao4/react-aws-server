0f2beed77ff3674507c0dba56a154b6aa1182bcf
pgao4 <pgao4@ualberta.ca>
2022-10-16 21:00:53 -0600

    add auth endpoints

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 const { registerEmailParams} = require('../helpers/email')
 const shortId = require('shortid')
+const expressJwt = require("express-jwt");
 
 AWS.config.update({
    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -114,5 +115,41 @@ exports.login = (req, res) => {
          token, 
          user: {_id, name, email, role}
       })
+   })
+}
+
+exports.requireSignin = expressJwt({secret: process.env.JWT_SECRET, algorithms: ['HS256']}) //req.user
+
+exports.authMiddleware = (req, res, next) => {
+   const authUserId = req.user._id
+   User.findOne({_id: authUserId}).exec((err, user)=>{
+      if(err || !user) {
+         return res.status(400).json({
+            error: "User not found"
+         })
+      }
+
+      req.profile = user
+      next()
+   })
+}
+
+exports.adminMiddleware = (req, res, next) => {
+   const adminUserId = req.user._id
+   User.findOne({_id: adminUserId}).exec((err, user)=>{
+      if(err || !user) {
+         return res.status(400).json({
+            error: "User not found"
+         })
+      }
+
+      if(user.role !== 'admin') {
+         return res.status(400).json({
+            error: "Admin resource Access denied"
+         })
+      }
+
+      req.profile = user
+      next()
    })
 }
